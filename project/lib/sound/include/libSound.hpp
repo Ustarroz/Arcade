c4c36550f2aabf81e868d8a5c821e614a39d2e87
Lucas TRONCY <lucas.troncy@epitech.eu>
2017-04-07 23:52:17 +0200

    sound begin

---
+++ project/lib/sound/include/libSound.hpp
@@ -0,0 +1,15 @@
+#ifndef LIBSOUND_HPP_
+# define LIBSOUND_HPP_
+
+namespace arcade
+{
+  class libSound
+  {
+    public:
+      libSound();
+      ~libSound();
+    private:
+  };
+}
+
+#endif
---
+++ project/lib/sound/src/libSound.cpp
@@ -0,0 +1,13 @@
+#include <SDL/SDL_mixer.h>
+#include "libSound.hpp"
+
+namespace arcade
+{
+  libSound::libSound()
+  {
+  }
+
+  libSound::~libSound()
+  {
+  }
+}
